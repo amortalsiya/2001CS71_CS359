c15fe05165c70b4b11bb180f6fba2b57e8e5c364
siyaram <siyaram_2001cs71@iitp.ac.in>
2023-04-23 21:12:40 +0530

    initial commit

---
+++ tut05/tut05.py
@@ -0,0 +1,13 @@
+
+from datetime import datetime
+start_time = datetime.now()
+
+
+def lsr():
+
+
+lsr()
+
+#This shall be the last lines of the code.
+end_time = datetime.now()
+print('Duration of Program Execution: {}'.format(end_time - start_time))
